c551bc9bd7a13cc7034cc442dcd0091a0646de27
Eyob Ayalew <eyobayalewwmed@gmail.com>
2023-06-08 22:10:26 +0300

    changes made

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,8 @@
 module emoloyeeManagmentSystem {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
